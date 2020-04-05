5007de3a28f5f9425046d424b2d48567b0a88262
bryan <bryan@LAPTOP-69IO348L>
2020-04-05 20:05:15 -0300

    Card e CardValidadtion

--- comanda-digital/src/main/java/br/com/comandadigital/constants/entity/CardValidation.java
+++ comanda-digital/src/main/java/br/com/comandadigital/constants/entity/CardValidation.java
@@ -3,5 +3,5 @@ package br.com.comandadigital.constants.entity;
 public class CardValidation {
     public static final String TABLE_NUMBER_VALIDATION_MESSAGE        =         "Numero da mesa invalido";
     public static final String AMOUNT_PEOPLE_VALIDATION_MESSAGE       =         "Numero de pessoas na mesa invalido";
-    public static final String DESCRIPTION_VALIDATION_MESSAGE         =         "Descricao invalida";
+
 }
--- comanda-digital/src/main/java/br/com/comandadigital/model/Card.java
+++ comanda-digital/src/main/java/br/com/comandadigital/model/Card.java
@@ -2,12 +2,50 @@ package br.com.comandadigital.model;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import br.com.comandadigital.constants.entity.CardValidation;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Data
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "tb_card")
 public class Card {
 
+	@Id
+    @GeneratedValue
     private Long idCard;
+	
+	@NotBlank(message = CardValidation.TABLE_NUMBER_VALIDATION_MESSAGE)
+	@Size(min = 1, max = 3, message = CardValidation.TABLE_NUMBER_VALIDATION_MESSAGE)
     private Long tableNumber;
+	
+	@NotBlank(message = CardValidation.TABLE_NUMBER_VALIDATION_MESSAGE)
+	@Size(min = 1, max = 3, message = CardValidation.AMOUNT_PEOPLE_VALIDATION_MESSAGE)
     private int amountPeople;
+    
     private Date beginDate;
+    
+
     private Date endDate;
+    
 
 }
