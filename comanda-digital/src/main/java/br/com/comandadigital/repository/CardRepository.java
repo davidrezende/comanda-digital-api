afc755490b77b889b79dfe3bd68e15920227e838
davidrezende <davidpenalives@gmail.com>
2020-05-03 03:09:13 -0300

    fixando card controller

--- comanda-digital/src/main/java/br/com/comandadigital/repository/CardRepository.java
+++ comanda-digital/src/main/java/br/com/comandadigital/repository/CardRepository.java
@@ -1,10 +1,12 @@
 package br.com.comandadigital.repository;
 
-
-
+import br.com.comandadigital.model.Store;
 import org.springframework.data.repository.PagingAndSortingRepository;
+
 import br.com.comandadigital.model.Card;
 
+import java.util.List;
+
 public interface CardRepository extends PagingAndSortingRepository<Card, Long> {
-	
+    List<Card> findAll();
 }
