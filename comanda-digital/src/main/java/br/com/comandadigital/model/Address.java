ddfdac1e4a7ad5ed722bf8f6b824313439aed391
davidrezende <davidpenalives@gmail.com>
2020-06-23 03:46:58 -0300

    Correcao nos nomes dos atributos

--- comanda-digital/src/main/java/br/com/comandadigital/model/Address.java
+++ comanda-digital/src/main/java/br/com/comandadigital/model/Address.java
@@ -21,37 +21,37 @@ import javax.validation.constraints.NotNull;
 @Table(name = "tb_address")
 public class Address {
 
-    @ApiModelProperty(value = "Código da comanda")
+    @ApiModelProperty(value = "Código do Endereço")
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long idAddress;
 
-    @ApiModelProperty(value = "Número do CEP do Usuário")
+    @ApiModelProperty(value = "Número do CEP do Estabelecimento")
     @NotBlank(message = AddressValidation.CEP_MESSAGE)
     @JsonProperty("cep")
     private String zipCode;
 
-    @ApiModelProperty(value = "Logradouro do Usuário")
+    @ApiModelProperty(value = "Logradouro do Estabelecimento")
     @NotBlank(message = AddressValidation.LOGRADOURO_MESSAGE)
     @JsonProperty("logradouro")
     private String streetName;
 
-    @ApiModelProperty(value = "Complemento do Usuário")
+    @ApiModelProperty(value = "Complemento do Estabelecimento")
     @NotBlank(message = AddressValidation.COMPLEMENTO_MESSAGE)
     @JsonProperty("complemento")
     private String aditionalInfo;
 
-    @ApiModelProperty(value = "Bairro do Usuário")
+    @ApiModelProperty(value = "Bairro do Estabelecimento")
     @NotBlank(message = AddressValidation.BAIRRO_MESSAGE)
     @JsonProperty("bairro")
     private String neighborhood;
 
-    @ApiModelProperty(value = "Cidade do Usuário")
+    @ApiModelProperty(value = "Cidade do Estabelecimento")
     @NotBlank(message = AddressValidation.CIDADE_MESSAGE)
     @JsonProperty("localidade")
     private String city;
 
-    @ApiModelProperty(value = "Estado do Usuário")
+    @ApiModelProperty(value = "Estado do Estabelecimento")
     @NotBlank(message = AddressValidation.UF_MESSAGE)
     @JsonProperty("uf")
     private String state;
